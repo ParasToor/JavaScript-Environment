6435e0b1e0974ad6d2f509e6492695fe183c779a
ParasToor <137055092+ParasToor@users.noreply.github.com>
2025-02-17 05:14:34 +0000

    02 dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553;
+let accountEmail = "paras@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+
+// accountId = 2;
+accountEmail = "hc@hc.com"
+accountPassword = "12112"
+accountCity = "bengaluru"
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+let name = "paras";
+console.log(typeof(name));
+
+//type of null is object
+console.log(typeof null);
+
+
+console.log(typeof undefined);
